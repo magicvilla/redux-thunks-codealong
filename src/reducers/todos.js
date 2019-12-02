c2e1b02475b22c33cb858043a6dd8555db796887
Damien <mail@damientimewell.com>
2019-12-02 13:50:42 +0100

    Writing a thunk which dispatches actions to both reducers

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { ui } from './ui'
 
 export const todos = createSlice({
   name: 'todos',
@@ -11,3 +12,15 @@ export const todos = createSlice({
     }
   }
 })
+
+export const fetchTodos = () => (
+  async (dispatch) => {
+    dispatch(ui.actions.setLoading(true))
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => res.json())
+      .then((json) => {
+        dispatch(todos.actions.setTodos(json))
+        dispatch(ui.actions.setLoading(false))
+      })
+  }
+)
